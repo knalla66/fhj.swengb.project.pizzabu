a669a877958a1d4643fbe157d51c1bbb6998dac4
knalla66 <markus.knaller@edu.fh-joanneum.at>
2016-01-04 16:31:35 +0100

    the machines are able to produce a product and the player can take the product from the machine (product is removed from the map)
    also a status (def ready) is implemented

--- src/main/scala/fhj/swengb/project/App.scala
+++ src/main/scala/fhj/swengb/project/App.scala
@@ -1,10 +1,39 @@
 package fhj.swengb.project
 
+import scala.concurrent.Future
+
 /**
  * @author ${user.name}
  */
 object App {
   def main(args: Array[String]) {
-    print("hallo")
+
   }
 }
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+
+object ScalaFuture {
+
+  def main(args: Array[String]) {
+
+    val f: Future[String] = Future {
+      Thread.sleep(2000)
+      "future value"
+    }
+
+    val f2 = f.map {
+      case s => {
+        println("OK!")
+        println("OK!")
+      }
+    }
+
+    Await.ready(f2, 60 seconds)
+    println("exit")
+  }
+}
+
