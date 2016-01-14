479ab53e47d488fd431badd0ece430fe1bf274cd
danielkandlhofer <daniel.kandlhofer@edu.fh-joanneum.at>
2016-01-14 19:13:48 +0100

    random guest order and received products

--- src/main/scala/fhj/swengb/project/Guest.scala
+++ src/main/scala/fhj/swengb/project/Guest.scala
@@ -3,21 +3,15 @@ package fhj.swengb.project
 import scala.util.Random
 
 /**
-  * Created by Stefan Leitner on 14.01.2016.
+  * Created by IMA14/PizzaBu on 14.01.2016.
   */
 trait Guest {
 
   val products:List[Product] = List(Pizza,Cola,CurlyFries)
 
-  def createOrder(howMany:Int = Random.shuffle(1 to 3).head, products: List[Product] = products,orderList: List[Product]=Nil): Seq[Product] = howMany match {
+  def createOrder(howMany:Int = Random.shuffle(List(1,2,3)).head, products: List[Product] = products,orderList: List[Product]=Nil): Seq[Product] = howMany match {
     case howMany if howMany > 0 => createOrder(howMany-1,products,orderList.::(Random.shuffle(products).head))
     case _ => orderList
-  }
-
-  def order1(howMany: Int, bestellung: List[Product],orderList: List[Product]=Nil): Unit = howMany match {
-    case howMany if howMany > 0 => order1(howMany-1,bestellung,orderList.::(Random.shuffle(bestellung).head))
-    case _ => orderList
-      println(orderList)
   }
 
   val order:Seq[Product] = createOrder()
@@ -35,23 +29,37 @@ object Guest2 extends Guest
 object Guest3 extends Guest
 object Guest4 extends Guest
 
-case class Guests(delieverable: Product) extends Guest {
-  Guest1.order
+object Guests {
+  def main(args: Array[String]) {
+    val g1 = Guests
+  }
+
+ val order = Guest1.order
+  println(order)
+  Guests(order)
+
 }
 
-object Test {
-  def main(args: Array[String]) {
-    println("G1")
-    println(Guest1.order)
-    println("G2")
-    println(Guest2.order)
-    println("G3")
-    println(Guest3.order)
-    println("G4")
-    println(Guest4.order)
-    println(Guest1.order)
-    println(Guest1.angry)
-    Guest1.setAngryLevel(3)
-    println(Guest1.angry)
+case class Guests(order:Seq[Product], recList:Seq[Product] = Nil) {
+
+  val compare:Unit =  if (order.diff(recList) == Nil ) {
+    //guest please leave the pizza-bu
+    println("alles bekommen")
+      }
+  else {
+    // wait for the next received product
+    println("warte auf weitere Produkte")
+    getProduct(readLine("Gib ein Produkt ein: "))
+  }
+
+  def getProduct(del:String) = del match {
+    case pizza if pizza == "pizza" => savereceived(Pizza,recList,order)
+    case cola if cola == "cola" => savereceived(Cola,recList,order)
+    case fries if del == "fries" => println("Fries");savereceived(CurlyFries,recList,order)
+  }
+
+    def savereceived(product:Product,recList:Seq[Product]=Nil,order:Seq[Product]) =  {
+      println(recList.+:(product)); Guests(order, recList.+:(product))//wait for received product (getProduct)
     }
+
 }
